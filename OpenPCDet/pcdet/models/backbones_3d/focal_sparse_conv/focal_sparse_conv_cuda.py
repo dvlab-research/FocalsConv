61313df69e5f933718f07b6c27f8463007791c88
yukang <yukangchen@cse.cuhk.edu.hk>
2023-02-23 17:58:43 +0800

    Add files via upload

--- OpenPCDet/pcdet/models/backbones_3d/focal_sparse_conv/focal_sparse_conv_cuda.py
+++ OpenPCDet/pcdet/models/backbones_3d/focal_sparse_conv/focal_sparse_conv_cuda.py
@@ -2,22 +2,29 @@ import torch
 import torch.nn as nn
 import spconv.pytorch as spconv
 from pcdet.ops.roiaware_pool3d.roiaware_pool3d_utils import points_in_boxes_gpu
-from pcdet.models.backbones_3d.focal_sparse_conv.utils import split_voxels, check_repeat, FocalLoss
+from pcdet.models.backbones_3d.focal_sparse_conv.focal_sparse_utils import split_voxels, check_repeat, check_repeat2, FocalLoss
 from pcdet.utils import common_utils
+from spconv.core import ConvAlgo
+import time
+from pcdet.datasets.processor.data_processor import VoxelGeneratorWrapper
+from spconv.pytorch.cppcore import torch_tensor_to_tv
+from spconv.utils import Point2VoxelGPU3d
+from pcdet.ops.check_repeat.check_repeat_utils import CheckRepeat
+from spconv.pytorch.spatial import RemoveDuplicate
 
-
-class FocalSparseConv(spconv.SparseModule):
+class FocalSparseConvCUDA(spconv.SparseModule):
     expansion = 1
 
     def __init__(self, inplanes, planes, voxel_stride, norm_fn=None, indice_key=None,
                 image_channel=3, kernel_size=3, padding=1, mask_multi=False, use_img=False,
                 topk=False, threshold=0.5, skip_mask_kernel=False, enlarge_voxel_channels=-1, 
-                point_cloud_range=[-3, -40, 0, 1, 40, 70.4], fuse_sum=True,
+                point_cloud_range=[-3, -40, 0, 1, 40, 70.4],
                 voxel_size = [0.1, 0.05, 0.05]):
-        super(FocalSparseConv, self).__init__()
+        super(FocalSparseConvCUDA, self).__init__()
 
-        self.conv = spconv.SubMConv3d(inplanes, planes, kernel_size=kernel_size, stride=1, bias=False, indice_key=indice_key)
-        self.bn1 = norm_fn(planes if fuse_sum else 2 * planes)
+        # SubMConv3d, FocalsConv3d
+        self.conv = spconv.FocalsConv3d(inplanes, planes, kernel_size=kernel_size, stride=1, bias=False, indice_key=indice_key, algo=ConvAlgo.Native)
+        self.bn1 = norm_fn(planes)
         self.relu = nn.ReLU(True)
         offset_channels = kernel_size**3
 
@@ -28,7 +35,9 @@ class FocalSparseConv(spconv.SparseModule):
         self.mask_multi = mask_multi
         self.skip_mask_kernel = skip_mask_kernel
         self.use_img = use_img
-        self.fuse_sum = fuse_sum
+        self.checkrepeat = CheckRepeat()
+        self.time = [0, 0, 0, 0]
+        self.indice_key = indice_key
 
         voxel_channel = enlarge_voxel_channels if enlarge_voxel_channels>0 else inplanes
         in_channels = image_channel + voxel_channel if use_img else voxel_channel
@@ -47,6 +56,7 @@ class FocalSparseConv(spconv.SparseModule):
         self.inv_idx =  torch.Tensor([2, 1, 0]).long().cuda()
         self.point_cloud_range = torch.Tensor(point_cloud_range).cuda()
         self.voxel_size = torch.Tensor(voxel_size).cuda()
+        self.remove_repeat_spconv = RemoveDuplicate()
 
     def construct_multimodal_features(self, x, x_rgb, batch_dict, fuse_sum=False):
         """
@@ -101,7 +111,7 @@ class FocalSparseConv(spconv.SparseModule):
             voxels_2d_int_list.append(voxels_2d_int)
 
             image_features_batch = torch.zeros((voxel_features_sparse.shape[0], x_rgb_batch.shape[0]), device=x_rgb_batch.device)
-            image_features_batch[filter_idx] = x_rgb_batch[:, voxels_2d_int[:, 1], voxels_2d_int[:, 0]].permute(1, 0).contiguous()
+            image_features_batch[filter_idx] = x_rgb_batch[:, voxels_2d_int[:, 1], voxels_2d_int[:, 0]].permute(1, 0)
 
             if fuse_sum:
                 image_with_voxelfeature = image_features_batch + voxel_features_sparse
@@ -123,10 +133,9 @@ class FocalSparseConv(spconv.SparseModule):
                 voxels_3d: [N, 3], the 3d positions of voxel centers
         """
         batch_size = x.batch_size
-        voxel_features_fore = []
-        voxel_indices_fore = []
-        voxel_features_back = []
-        voxel_indices_back = []
+        add_features = []
+        add_indices = []
+        add_masks = []
 
         box_of_pts_cls_targets = []
         mask_voxels = []
@@ -143,23 +152,17 @@ class FocalSparseConv(spconv.SparseModule):
                 box_of_pts_batch = points_in_boxes_gpu(voxels_3d_batch[:, :, self.inv_idx], gt_boxes).squeeze(0)
                 box_of_pts_cls_targets.append(box_of_pts_batch>=0)
 
-            features_fore, indices_fore, features_back, indices_back, mask_kernel = split_voxels(x, b, imps_3d, voxels_3d, self.kernel_offsets, mask_multi=self.mask_multi, topk=self.topk, threshold=self.threshold)
+            selected_features, selected_indices, mask_kernel_fore = split_voxels(x, b, imps_3d, voxels_3d, self.kernel_offsets, 
+                                    mask_multi=self.mask_multi, topk=self.topk, threshold=self.threshold, only_return_add=True)
 
-            mask_kernel_list.append(mask_kernel)
-            voxel_features_fore.append(features_fore)
-            voxel_indices_fore.append(indices_fore)
-            voxel_features_back.append(features_back)
-            voxel_indices_back.append(indices_back)
+            add_features.append(selected_features)
+            add_indices.append(selected_indices)
+            add_masks.append(mask_kernel_fore)
 
-        voxel_features_fore = torch.cat(voxel_features_fore, dim=0)
-        voxel_indices_fore = torch.cat(voxel_indices_fore, dim=0)
-        voxel_features_back = torch.cat(voxel_features_back, dim=0)
-        voxel_indices_back = torch.cat(voxel_indices_back, dim=0)
-        mask_kernel = torch.cat(mask_kernel_list, dim=0)
-
-        x_fore = spconv.SparseConvTensor(voxel_features_fore, voxel_indices_fore, x.spatial_shape, x.batch_size)
-        x_back = spconv.SparseConvTensor(voxel_features_back, voxel_indices_back, x.spatial_shape, x.batch_size)
-
+        add_features = torch.cat(add_features, dim=0)
+        add_indices = torch.cat(add_indices, dim=0)
+        add_masks = torch.cat(add_masks, dim=0)
+        
         loss_box_of_pts = 0
         if self.training:
             mask_voxels = torch.cat(mask_voxels)
@@ -167,36 +170,44 @@ class FocalSparseConv(spconv.SparseModule):
             mask_voxels_two_classes = torch.cat([1-mask_voxels.unsqueeze(-1), mask_voxels.unsqueeze(-1)], dim=1)
             loss_box_of_pts = self.focal_loss(mask_voxels_two_classes, box_of_pts_cls_targets.long())
 
+        return add_features, add_indices, add_masks, loss_box_of_pts
+
+        x_fore = spconv.SparseConvTensor(voxel_features_fore, voxel_indices_fore, x.spatial_shape, x.batch_size)
+        x_back = spconv.SparseConvTensor(voxel_features_back, voxel_indices_back, x.spatial_shape, x.batch_size)
+
         return x_fore, x_back, loss_box_of_pts, mask_kernel
 
-    def combine_out(self, x_fore, x_back, remove_repeat=False):
-        """
-            Combine the foreground and background sparse features together.
-            Args:
-                x_fore: [N1, C], foreground sparse features
-                x_back: [N2, C], background sparse features
-                remove_repeat: bool, whether to remove the spatial replicate features.
-        """
-        x_fore_features = torch.cat([x_fore.features, x_back.features], dim=0)
-        x_fore_indices = torch.cat([x_fore.indices, x_back.indices], dim=0)
-
-        if remove_repeat:
-            index = x_fore_indices[:, 0]
-            features_out_list = []
-            indices_coords_out_list = []
-            for b in range(x_fore.batch_size):
-                batch_index = index==b
-                features_out, indices_coords_out, _ = check_repeat(x_fore_features[batch_index], x_fore_indices[batch_index], flip_first=False)
-                features_out_list.append(features_out)
-                indices_coords_out_list.append(indices_coords_out)
-            x_fore_features = torch.cat(features_out_list, dim=0)
-            x_fore_indices = torch.cat(indices_coords_out_list, dim=0)
-
+    def combine_out2(self, x_fore, add_features, add_indices, remove_repeat=False, replace_features=True):
+        #x_fore_features = torch.cat([x_fore.features, add_features], dim=0)
+        x_fore_indices = torch.cat([x_fore.indices, add_indices], dim=0)
+        x_fore_features = torch.cat([x_fore.features, torch.zeros((add_indices.shape[0], x_fore.features.shape[-1]), device=x_fore.features.device)])
         x_fore = x_fore.replace_feature(x_fore_features)
         x_fore.indices = x_fore_indices
+        #im_mask = x_fore_features.abs().sum(-1)>0
+        return x_fore #, im_mask
 
-        return x_fore
-        
+    def _unique(self, out):
+        features, indices = out.features, out.indices
+        X, Y, Z = out.spatial_shape
+        idx = indices
+        idx_sum = idx.select(1, 0) * X*Y*Z + idx.select(1, 1) * Y*Z + idx.select(1, 2) * Z + idx.select(1, 3)
+        _, ind = idx_sum.sort()
+        indices = indices[ind] 
+        features = features[ind]
+        #features, indices = features.flip([0]), indices.flip([0])
+        _unique, inverse = torch.unique_consecutive(indices, return_inverse=True, dim=0)
+
+        if _unique.shape[0] < indices.shape[0]:
+            perm = torch.arange(inverse.size(0), dtype=inverse.dtype, device=inverse.device)
+            features_new = torch.zeros((_unique.shape[0], features.shape[-1]), device=features.device)
+            features_new.index_add_(0, inverse.long(), features)
+            features = features_new
+            perm_ = inverse.new_empty(_unique.size(0)).scatter_(0, inverse, perm)
+            indices = indices[perm_].int()
+        out = out.replace_feature(features)
+        out.indices = indices
+        return out
+
     def forward(self, x, batch_dict, x_rgb=None):
         spatial_indices = x.indices[:, 1:] * self.voxel_stride
         voxels_3d = spatial_indices * self.voxel_size + self.point_cloud_range[:3]
@@ -209,18 +220,33 @@ class FocalSparseConv(spconv.SparseModule):
 
         imps_3d = self.conv_imp(x_predict).features
 
-        x_fore, x_back, loss_box_of_pts, mask_kernel = self._gen_sparse_features(x, imps_3d, batch_dict, voxels_3d)
+        add_features, add_indices, add_masks, loss_box_of_pts = self._gen_sparse_features(x, imps_3d, batch_dict, voxels_3d)
 
         if not self.skip_mask_kernel:
-            x_fore = x_fore.replace_feature(x_fore.features * mask_kernel.unsqueeze(-1))
-        out = self.combine_out(x_fore, x_back, remove_repeat=True)
-        out = self.conv(out)
+            add_features *= add_masks.unsqueeze(-1) 
+
+        add_indices = torch.unique(add_indices, dim=0)
+
+        out = self.combine_out2(x, add_features, add_indices, remove_repeat=True)
+
+        if out.batch_size >1:
+            out_list = []
+            for b in range(out.batch_size):
+                batch_index = out.indices[:, 0] == b
+                ori_feat_num = (x.indices[:, 0] == b).sum()
+                x_ = spconv.SparseConvTensor(out.features[batch_index], out.indices[batch_index], out.spatial_shape, out.batch_size)
+                out_ = self.conv(x_, ori_feat_num=ori_feat_num) #x_.features.shape[0])
+                out_list.append(out_)
+            out = out.replace_feature(torch.cat([out_.features for out_ in out_list]))
+            out.indices = torch.cat([out_.indices for out_ in out_list])
+        else:
+            out = self.conv(out, ori_feat_num=int(x.features.shape[0]))
+        out = self._unique(out)
 
         if self.use_img:
-            out = out.replace_feature(self.construct_multimodal_features(out, x_rgb, batch_dict, self.fuse_sum))
+            out = out.replace_feature(self.construct_multimodal_features(out, x_rgb, batch_dict, True))
 
         out = out.replace_feature(self.bn1(out.features))
         out = out.replace_feature(self.relu(out.features))
 
-        batch_dict['loss_box_of_pts'] += loss_box_of_pts
-        return out, batch_dict
+        return out, batch_dict, loss_box_of_pts
